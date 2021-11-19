ae6107d347e1e3fb4a7139cbd4882ad6a7f7fd38
wellapos <wellington258@live.com>
2021-11-19 18:29:44 -0300

    post cats

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
     def index
         @posts = Post.order(:name)
         @users = User.order(:email)
@@ -7,13 +8,20 @@ class PostsController < ApplicationController
     def new
         @post = Post.new
         @users = User.order(:email)
+        @categories = Category.order(:name)
+        @post_cats = @post.post_cats.select(:category_id)
     end
 
     def create
         @post = Post.new(post_params)
-        @users = User.order(:email)
     
         if @post.save
+            if params[:category_ids].present?
+                params[:category_ids].each do |id|
+                    PostCat.create(post_id: @post.id, category_id: id)
+                end
+            end
+            
             redirect_to posts_path
         else
             render :new, status: :unprocessable_entity
@@ -23,12 +31,28 @@ class PostsController < ApplicationController
     def edit
         @post = Post.find(params[:id])
         @users = User.order(:email)
+        @post_cats = @post.post_cats.select(:category_id)
+        @categories = Category.order(:name)
     end
 
     def update
         @post = Post.find(params[:id])
-    
+        
         if @post.update(post_params)
+            @post_cats_delete = PostCat.where(post_id: @post.id).where('category_id NOT IN (:category_ids)', category_ids: params[:category_ids])
+            @post_cats_delete.delete_all
+
+            if params[:category_ids].present?
+                params[:category_ids].each do |id|
+                    post_cat = PostCat.where(post_id: @post.id, category_id: id)
+                    if post_cat.blank?
+                        PostCat.create(post_id: @post.id, category_id: id)
+                    end
+                end
+            else
+                @post.post_cats.destroy_all
+            end
+
             redirect_to posts_path
         else
             render :edit, status: :unprocessable_entity
@@ -43,9 +67,10 @@ class PostsController < ApplicationController
         end
         redirect_to posts_path
     end
-    
+
     private
     def post_params
         params.require(:post).permit(:name, :summary, :content, :user_id, :image)
     end
+
 end
