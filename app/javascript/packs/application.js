64200ba0b976e4ab76b2b772e0cada90f47a7ec4
wellapos <wellington258@live.com>
2021-12-21 17:29:04 -0300

    heroko alterações css5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,6 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-var jQuery = require("jquery")
-global.$ = global.jQuery = jQuery
-window.$ = window.jQuery = jQuery
 
 
 import("../material-dashboard.js")
